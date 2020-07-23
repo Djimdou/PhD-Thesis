18ef82307512c6585e8e80a383e10975c1543074
Magloire LOUDEGUI DJIMDOU <50971645+Djimdou@users.noreply.github.com>
2020-07-23 18:36:54 -0400

    Add files via upload

--- Chapter2.R
+++ Chapter2.R
@@ -1,27 +1,69 @@
-# # CHAPTER 2: ESTIMATION OF THE GENERATOR
+# # # # CHAPTER 2: ESTIMATION OF THE GENERATOR
 
 
-# Librairies
+# # Preps
 
 library(copula) # for claytonCopula
 
+n = 1000
 
-n = 200
+W = seq(from=0.01,to=1,by=0.001)
+
+# # Simulating values from Archimedean copulas
+
+# Independence copula
+
+indep <- indepCopula(dim=2)
+U <- rCopula(n=n, indep)[,1]
+V <- rCopula(n=n, indep)[,2]
+
+Phi_true = -log(W)
 
 # Clayton copula
 
-clayton <- claytonCopula(param=2)
+theta = 0.25
+clayton <- claytonCopula(param=theta,dim=2)
 U <- rCopula(n=n, clayton)[,1]
 V <- rCopula(n=n, clayton)[,2]
 
-# Weibull distribution for X and Y
+Phi_true = (W**(-theta)-1)/theta
+
+# AMH copula
+
+theta = 0.5
+AMH <- amhCopula(param=theta,dim=2)
+U <- rCopula(n=n, AMH)[,1]
+V <- rCopula(n=n, AMH)[,2]
+
+Phi_true = log((1-theta*(1-W))/W)/(1-theta)
+
+# Frank
+
+theta = 1
+frank <- frankCopula(param=theta,dim=2)
+U <- rCopula(n=n, frank)[,1]
+V <- rCopula(n=n, frank)[,2]
+
+Phi_true = -((exp(theta)-1)/theta)*log((exp(-theta*W)-1)/(exp(-theta)-1))
+
+# Gumbel (counter-example)
+
+theta = 10
+gumbel <- gumbelCopula(param=theta,dim=2)
+U <- rCopula(n=n, gumbel)[,1]
+V <- rCopula(n=n, gumbel)[,2]
+
+Phi_true = (-log(W))**theta
+
+# # Weibull distribution for X and Y
+
 alpha = 10 # Weibull distribution shape parameter
 beta = 1.7 # Weibull distribution scale parameter
 
 X = beta*(-log(U))**(1/alpha)
 Y = beta*(-log(V))**(1/alpha)
 
-# Pseudo-sample
+# # Pseudo-sample
 
 V = rep(NA,times=n)
 
@@ -29,19 +71,18 @@ for(i in 1:n){
   V[i] = (sum((X <= X[i])*(Y <= Y[i])))/(n-1)
 }
 
-# Empirical distribution
+# # Empirical distribution
 
 Kn = ecdf(V)
 Vn = unique(V[order(V)])
 
-# # Estimator 1: differential equation
+# # # Estimator 1: differential equation
 
-W = seq(from=0,to=1,by=0.01)
-Phi = rep(NA,times=length(W))
+Phi_est = rep(NA,times=length(W))
 
 for(i in 1:length(W)){
   
-  if(W[i]<=1-1/n){
+  if(W[i]< 1-1/n){
     T = unique(c(W[i],Vn[which((W[i] <= Vn) & (Vn <= 1-1/n))],1-1/n))
     ProdTerm = rep(NA,times=length(T)-1)
     for(j in 1:(length(T)-1)){
@@ -51,12 +92,26 @@ for(i in 1:length(W)){
         ProdTerm[j] = 1
       }
     }
-    Phi[i] = abs(prod(ProdTerm))/n
+    Phi_est[i] = abs(prod(ProdTerm))/n
     }else{
-    Phi[i] = 0
+      Phi_est[i] = 0
   }
 }
 
-plot(W,Phi,type='l')
+# # Graph
 
-# # Estimator 2: integral equation
+Ylim = range(c(Phi_est,Phi_true),na.rm = TRUE)
+
+plot(W,Phi_est,type='l',col='red') #,ylim=Ylim
+lines(W,Phi_true,col='blue')
+
+# # # Estimator 2: integral equation
+
+h = rep(NA,times=length(Vn))
+h[length(Vn)] = 1 
+
+for(i in (length(Vn)-1):1){
+  h[i] = (1/(n*Kn(Vn[i])-i))*sum(h[(i+1):length(Vn)])
+}
+
+Phi = -cumsum(diff(h))
