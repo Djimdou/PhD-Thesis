5a4d70829efd39b2d268ffb91cb8856513ca1b73
Magloire LOUDEGUI DJIMDOU <50971645+Djimdou@users.noreply.github.com>
2020-10-13 17:43:59 -0400

    Add files via upload

--- Chapter2.R
+++ Chapter2.R
@@ -5,9 +5,9 @@
 
 library(copula) # for claytonCopula
 
-n = 1000
+m = 1000
 
-W = 1:n/n#seq(from=1/n,to=1,by=0.01)
+W = 1:m/m #seq(from=1/n,to=1,by=0.01) # grid for the x-axis
 
 # # Simulating values from Archimedean copulas
 
@@ -18,19 +18,19 @@ Phi_true = -log(W)
 
 # Clayton copula
 
-theta = 3
+theta = 1
 cop <- claytonCopula(param=theta,dim=2)
 Phi_true = (W**(-theta)-1)/theta
 
 # AMH copula
 
-theta = 0.5
+theta = 0.75
 cop <- amhCopula(param=theta,dim=2)
 Phi_true = log((1-theta*(1-W))/W)/(1-theta)
 
 # Frank
 
-theta = 1
+theta = -1
 cop <- frankCopula(param=theta,dim=2)
 Phi_true = -((exp(theta)-1)/theta)*log((exp(-theta*W)-1)/(exp(-theta)-1))
 
@@ -40,7 +40,9 @@ theta = 10
 cop <- gumbelCopula(param=theta,dim=2)
 Phi_true = (-log(W))**theta
 
-# # Weibull distribution for X and Y
+# # Sampling X and Y
+
+n = 500 # size of sample
 
 alpha = 2 # Weibull distribution shape parameter
 beta = 2 # Weibull distribution scale parameter
@@ -49,7 +51,7 @@ MyCopula <- mvdc(copula=cop, # copula for (F(X), F(Y))
                  margins=c("weibull","weibull"), # Weibull distribution for margins X and Y
                  paramMargins=list(shape=alpha,scale=beta)) # alpha:shape, beta:scale
 
-set.seed(1)
+#set.seed(1)
 XY <- rMvdc(n=n,MyCopula)
 X <- XY[,1]
 Y <- XY[,2]
@@ -69,31 +71,36 @@ Vn = V[order(V)]
 
 # # # Estimator 1: differential equation
 
-Phi_est = rep(NA,times=length(W))
+Phi_est = rep(NA,times=n)
 
-for(i in 1:length(W)){
-  
-  if(W[i]< 1-1/n){
-    T = unique(c(W[i],Vn[which((W[i] <= Vn) & (Vn <= 1-1/n))],1-1/n))
+for(i in 1:n){
+  if(Vn[i] < 1-1/n){
+    #T = unique(c(W[i],Vn[which((W[i] <= Vn) & (Vn <= 1-1/n))],1-1/n))
+    T = unique(c(Vn[which((Vn[i] <= Vn) & (Vn <= 1-1/n))],1-1/n))
     ProdTerm = rep(NA,times=length(T)-1)
-    for(j in 1:(length(T)-1)){
-      if(Kn(T[j])!=T[j]){
-        ProdTerm[j] = (Kn(T[j])-T[j])/(Kn(T[j])-T[j+1])
-      }else{
-        ProdTerm[j] = 1
-      }
-    }
-    Phi_est[i] = abs(prod(ProdTerm))/n
+    #for(j in 1:(length(T)-1)){
+      #if((Kn(T[j])!=T[j])){# & (Kn(T[j])!=T[j+1])
+      #  ProdTerm[j] = (Kn(T[j])-T[j])/(Kn(T[j])-T[j+1])
+      #}else{
+      #  ProdTerm[j] = 1
+      #}
+    #}
+    ProdTerm = (Kn(T[-length(T)])-T[-length(T)])/(Kn(T[-length(T)])-T[-1])
+    ProdTerm = ProdTerm[is.finite(ProdTerm) & (ProdTerm != 0)]
+    Phi_est[i] = (1/n)*abs(prod(ProdTerm))
     }else{
       Phi_est[i] = 0
   }
 }
 
+Phi_est_plot = approx(x=c(0,Vn,1), y=c(Phi_est[1],Phi_est,0), xout=W, method="constant", ties = mean)$y 
+
+
 # # Graph
 
-Ylim = range(c(Phi_est,Phi_true),na.rm = TRUE)
+Ylim = range(c(Phi_est_plot,Phi_true),na.rm = TRUE)
 
-plot(W,Phi_est,type='l',col='red',ylim=Ylim) #
+plot(W,Phi_est_plot,type='l',col='red',ylim=Ylim)
 lines(W,Phi_true,col='blue')
 
 # # # Estimator 2: integral equation
