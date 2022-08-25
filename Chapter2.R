55948652e58ebb8741f76c872e34a4b3dc4705e8
Magloire LOUDEGUI DJIMDOU <50971645+Djimdou@users.noreply.github.com>
2022-08-24 22:42:46 -0400

    Add files via upload

--- Chapter2.R
+++ Chapter2.R
@@ -3,6 +3,7 @@
 
 # # Preps: loading libraries
 
+#install.packages("copula")
 library(copula) # for claytonCopula
 
 
@@ -19,7 +20,7 @@ Phi_true = -log(W)
 
 # Clayton copula
 
-theta = 3
+theta = 2
 cop <- claytonCopula(param=theta,dim=2)
 Phi_true = (W**(-theta)-1)/theta
 
@@ -43,7 +44,7 @@ Phi_true = (-log(W))**theta
 
 # # Sampling X and Y
 
-n = 500 # size of sample
+n = 750 # size of sample
 
 alpha = 2 # Weibull distribution shape parameter
 beta = 2 # Weibull distribution scale parameter
@@ -76,28 +77,14 @@ Phi_est = rep(0,times=n)
 
 for(i in 1:n){
   if(Vn[i] < 1-1/n){
-    #T = unique(c(W[i],Vn[which((W[i] <= Vn) & (Vn <= 1-1/n))],1-1/n))
-    #T = unique(c(Vn[which((Vn[i] <= Vn) & (Vn <= 1-1/n))],1-1/n))
     T = unique(c(Vn[(Vn[i] <= Vn) & (Vn <= 1-1/n)],1-1/n))
     ProdTerm = rep(NA,times=length(T)-1)
-    #for(j in 1:(length(T)-1)){
-      #if((Kn(T[j])!=T[j])){# & (Kn(T[j])!=T[j+1])
-      #  ProdTerm[j] = (Kn(T[j])-T[j])/(Kn(T[j])-T[j+1])
-      #}else{
-      #  ProdTerm[j] = 1
-      #}
-    #}
     ProdTerm = (Kn(T[-length(T)])-T[-length(T)])/(Kn(T[-length(T)])-T[-1])
-    #ProdTerm = ProdTerm[is.finite(ProdTerm) & (ProdTerm != 0)]
     Phi_est[i] = (1/n)*abs(prod(ProdTerm))
-  #  }else{
-  #    Phi_est[i] = 0
   }
 }
 
-#Phi_est_diff = approx(x=c(0,Vn,1), y=c(Phi_est[1],Phi_est,0), xout=W, method="constant", ties = "ordered")$y 
 Phi_est_diff = approx(x=c(0,Vn,1), y=c(Phi_est[1],Phi_est,0), xout=W, method="constant", ties = "ordered")$y 
-
 
 
 # # # Estimator 2: integral equation
@@ -106,24 +93,19 @@ h = rep(NA,times=n)
 h[n] = 1 
 
 for(i in (n-1):1){
-#  if(Kn(i/n)-i/n > 0){
   h[i] = (1/(n*Kn(i/n)-i))*sum(h[(i+1):n])
-#  }else{
-#    h[i] = h[i+1]
-#  }
 }
 
 # Step interpolation
-#Phi_est= rep(NA,times=length(W))
 
 cond = h>=0 & is.finite(h) # may have 0's, when Kn(i/n)-i/n <= 0
 
 h.interp = approx(x=c(0,Vn[cond],1), y=c(h[cond][1],h[cond],1), xout=W, method="constant", ties = mean)$y 
 
-Phi_est_int = (sum(h.interp) - cumsum(c(0,h.interp[-n])))/n
+Phi_est_int = (sum(h.interp) - cumsum(c(0,h.interp[-length(h.interp)])))/n
 
 
-# # # Graph: comparision Phi_est_int, Phi_est_diff, Phi_true
+# # # Graph: comparison Phi_est_int, Phi_est_diff, Phi_true
 
 Ylim = range(c(Phi_est_int,Phi_est_diff,Phi_true),na.rm = TRUE)
 
@@ -143,7 +125,19 @@ axis(2, at=Ylabels,labels=Ylabels,las=1,font=2,hadj=1,padj=0)
 legend("topright",legend=c("true density","integral estimator","differential estimator"),lwd = 4,col=c("blue","red", "orange"),lty=1,cex=1.25,bty="n")
 
 
+# # # Graph (for Statistics Papers article): comparision to empirical copula process
 
+Kn_hat <- W+Phi_est_int/h.interp
+Ylim <- range(c(Kn_hat,W),na.rm = TRUE)
+
+plot(W,Kn(W),type="l",ylim=Ylim,xlab="t",ylab="Kn")
+lines(W,Kn_hat,col="red")
+
+# Write to csv (for LaTex)
+
+write.csv(x=data.frame(cbind(W,Kn_hat,Kn(W))),
+          file="C:/Users/djimd/OneDrive/Documents/Concordia - PhD/Thesis/Written Articles/Statistical Papers/clayton_empirical_kendall_750.csv",
+          row.names = FALSE)
 
 # # # # # # # # #  Limiting behavior of Estimator 1 (differential) # # # # # # # # # # # # # 
 
